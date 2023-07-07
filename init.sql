4c8a3be3f1e7f81adbe3e3dea69a34d1ec7bf883
Bao Phung <bao.future94@gmail.com>
2023-07-07 15:38:54 +0000

    update init.sql 3

--- init.sql
+++ init.sql
@@ -1,3 +1,8 @@
-CREATE DATABASE IF NOT EXISTS todolist;
+DO $$ BEGIN
+  IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'todolist') THEN
+    CREATE DATABASE todolist;
+  END IF;
+END $$;
+
 CREATE USER bao_todolist WITH PASSWORD 'baobao123';
 GRANT ALL PRIVILEGES ON DATABASE todolist TO bao_todolist;
