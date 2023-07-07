ee6fc163e39e152ba4e7fe7659f0d7686996ee47
Bao Phung <bao.future94@gmail.com>
2023-07-07 15:56:52 +0000

    update init.sql

--- init.sql
+++ init.sql
@@ -5,5 +5,5 @@ DO $$ BEGIN
   END IF;
 END $$;
 
-CREATE USER bao_todolist WITH PASSWORD 'baobao123';
+DROP USER IF EXISTS bao_todolist; CREATE USER bao_todolist WITH PASSWORD 'baobao123';
 GRANT ALL PRIVILEGES ON DATABASE todolist TO bao_todolist;
